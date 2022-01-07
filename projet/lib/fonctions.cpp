e60e61dea400b27617e2e1545a7855a6f7249bfa
Ronan Adam <adam.ronan39@gmail.com>
2022-01-07 10:11:01 +0100

    Debut implémentation raymarching

---
+++ projet/lib/fonctions.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+#include "fonctions.h"
+
+m = mat3(0.0f, 0.8f, 0.6f,
+        -0.8f, 0.36f, -0.48f,
+        -0.6f, -0.48f, 0.64f);
+
+float linterp(float a, float b, float t)
+{
+    return a+t*(b-a);
+}
+
+float hash(float n)
+{
+    float x = sin(n)*43758.5453;
+    return x-floor(x);
+}
+
+float noise (vec3 x)
+{
+    vec3 p = vec3(floor(x.x()),floor(x.y()),floor(x.z()));
+    vec3 f = x-p;
+
+    f = f*f*(3.0f-2.0f*f);
+
+    float n = p.x()+p.y()*57.0f+113.0f*p.z();
+
+    float res = linterp(
+        linterp(
+            linterp(hash(n+0.0f),hash(n+1.0f),f.x()),
+            linterp(hash(n+57.0f),hash(n+58.0f),f.x()),
+            f.y()),
+        linterp(
+            linterp(hash(n+113.0f),hash(n+114.0f),f.x()),
+            linterp(hash(n+170.0f),hash(n+171.0f),f.x()),
+            f.y()),
+        f.z())
+
+    return res;
+}
+
+float fbm(vec3 p)
+{
+    float f;
+    f  = 0.5000*noise( p ); p = m*p*2.02;
+    f += 0.2500*noise( p ); p = m*p*2.03;
+    f += 0.1250*noise( p );
+    return f;
+}
+
+float scene(vec3 p)
+{
+    float length = sqrt(pow(p.x(),2),pow(p.y(),2),pow(p.z(),2));
+    return 0.1f-length*0.5f+fbm(p*0.3);
+}
---
+++ projet/lib/fonctions.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "mat3.hpp"
+#include "vec3.hpp"
+
+mat3 m;
+
+float linterp(float a, float b, float t);
+
+float hash(float n);
+float noise(vec3 x);
+float fbm(vec3 p);
+float scene(vec3 p);
+
+
